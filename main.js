c30f43de6cb0bec439fa02bd892b7e24deab00b2
Valeriia Neskoromnaya <81985957+VNeskoromnaya@users.noreply.github.com>
2022-05-18 00:19:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+/* 
+3. Напишите функцию `formatDate(date)`, форматирующую `date` по следующему принципу: 
+    - Если спустя `date` прошло менее 1 секунды, вывести `"прямо сейчас"`.
+    - В противном случае, если с `date` прошло меньше 1 минуты, вывести `"n сек. назад"`.
+    - В противном случае, если меньше часа, вывести `"m мин. назад"`.
+    - В противном случае, полная дата в формате `"DD.MM.YY HH:mm"`. А именно: `"день.месяц.год часы:минуты"`, всё в виде двух цифр, т.е. `31.12.16 10:00`.
+    */
+
+
+function formatDate(date) {
+    let diff = new Date() - date;
+    
+    if( diff < 1000 ) {
+        console.log('Прямо сейчас');
+    } else if ( diff < 60000) {
+        console.log(`${Math.ceil(diff/1000)} cек назад`);
+    } else if ( diff < 3600000 ) {
+        console.log(`${Math.floor(diff/60000)} мин назад`);
+    } else {
+        let addZero = (num) => {
+            return num < 10 ? '0' + num : num;
+        };
+        console.log(`${addZero(date.getDate())}.${addZero(date.getMonth())}.${addZero(date.getFullYear() % 100)} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`)
+    }
+}
+
+//formatDate(date)
